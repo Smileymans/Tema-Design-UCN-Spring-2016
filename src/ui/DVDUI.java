244da816f875c4736d8e1f85db399810c93a9d41
Norup1234 <dickynorup@gmail.com>
2016-04-20 12:48:48 +0200

    Added DVDUI

--- src/ui/DVDUI.java
+++ src/ui/DVDUI.java
@@ -1,5 +1,91 @@
 package ui;
+import java.util.*;
+import ctr.*;
+import model.*;
+
 
 public class DVDUI {
 
+	private DVDCtr pCtr;
+	private SingleInput si;
+	
+	public DVDUI(){
+		pCtr = new DVDCtr();
+		si = new SingleInput();
+		start();
+	}
+	
+	
+	private void start(){
+		boolean goOn = true;
+		ListChoice<Integer> mainMenu = new ListChoice<>();
+		mainMenu.addOption("back", 0); //går til MainMenu
+		mainMenu.addOption("Add dvd", 1); //Tilføj ny dvd
+		mainMenu.addOption("View dvds", 2); //Liste over alle dvd
+		mainMenu.addOption("Update dvds", 3); //Opdater dvd
+		mainMenu.addOption("Delete dvd", 4); //Slet en dvd
+		
+		while(goOn){
+			Integer choice = mainMenu.input("Choose option", false); //showCancelOption bruges til false
+			if(choice == null){
+				choice = 0;
+			}
+			switch(choice){
+			case 0:
+				goOn = false;
+				exit();
+				break;
+			case 1:
+				addDvd();
+				break;
+			case 2:
+				viewDvds();
+				break;
+			case 3:
+				updateDvds();
+				break;
+			case 4:
+				deleteDvds();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+		private void exit(){
+			System.out.println("Test af exit");
+		}
+		
+		private void addDvd(){
+			System.out.println("Add dvd:");
+			String barcode = si.inputString(" Barcode: ");
+			String title = si.inputString(" Title: ");
+			String artist = si.inputString(" Artist: ");
+			String publicationDate = si.inputString(" PublicationDate: ");
+			String genre = si.inputString(" Genre: ");
+			DVD p = pCtr.createDvd(barcode, title, artist, publicationDate, genre);
+			viewDVD(p);
+			System.out.println();
+		}
+		private void viewDVD(DVD p){
+			System.out.println("DVD: " + p.toString());
+		}
+		
+		private void viewDvds(){
+			System.out.println("All dvds in the system: ");
+			List<DVD> dvds = pCtr.getAll();
+			for(DVD p : dvds){
+				System.out.println(" " + p);
+			}
+			System.out.println();
+		}
+		
+		private void updateDvds(){
+			System.out.println("Test af updateDvds");
+		}
+		
+		private void deleteDvds(){
+			System.out.println("Test af deleteDvds");
+		}
+	
 }
